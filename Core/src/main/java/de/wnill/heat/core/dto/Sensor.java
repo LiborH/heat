48b77015c819a786dc03a4b2d37b87a9076bab80
Wladi <mail@wnill.de>
2015-10-16 13:08:47 +0200

    first PoC test classes

---
+++ Core/src/main/java/de/wnill/heat/core/dto/Sensor.java
@@ -0,0 +1,21 @@
+package de.wnill.heat.core.dto;
+
+public class Sensor {
+
+	private String id;
+
+	public Sensor(String id) {
+		this.id = id;
+	}
+	
+	
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+	
+	
+}
---
+++ Core/src/main/java/de/wnill/heat/core/services/SensorService.java
@@ -0,0 +1,29 @@
+package de.wnill.heat.core.services;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import de.wnill.heat.core.dto.Sensor;
+
+public class SensorService {
+
+	private ArrayList<Sensor> sensors = new ArrayList<Sensor>();
+	
+	private static SensorService instance = null;
+	
+	public static SensorService getInstance() {
+		if (instance == null) {
+			instance = new SensorService();
+		}
+		return instance;
+	}
+	
+	public void addSensor(String id) {
+		sensors.add(new Sensor(id));
+	}
+	
+	public List<Sensor> getAll() {
+		return sensors;
+	}
+
+}
