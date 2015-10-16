c409d04d314255aa30d2a149b41f07b182707893
Wladi <mail@wnill.de>
2015-10-16 13:09:57 +0200

    including Rest Project

---
+++ Rest/src/de/wnill/heat/rest/SensorsFacade.java
@@ -0,0 +1,36 @@
+package de.wnill.heat.rest;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Response;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import de.wnill.heat.core.services.SensorService;
+
+@Path("/sensors")
+public class SensorsFacade {
+
+	@GET
+	@Produces("application/json")
+	public Response showSensors() throws JSONException {
+		JSONObject jsonObject = new JSONObject();
+		jsonObject.put("sensorIds", SensorService.getInstance().getAll());
+		return Response.status(200).entity(jsonObject.toString()).build();
+	}
+	
+	@Path("{id}")
+	@GET
+	@Produces("application/json")
+	public Response addSensor(@PathParam("id") String id) {
+		SensorService.getInstance().addSensor(id);
+		JSONObject jsonObject = new JSONObject();
+		jsonObject.put("id", id);
+		return Response.status(200).entity(jsonObject.toString()).build();
+
+	}
+
+}
