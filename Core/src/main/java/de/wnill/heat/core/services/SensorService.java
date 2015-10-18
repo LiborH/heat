056cc7cd7c3857a20a4dc1da55d31de36613c125
Wladi <mail@wnill.de>
2015-10-18 12:01:12 +0200

    added google style formatter

--- Core/src/main/java/de/wnill/heat/core/services/SensorService.java
+++ Core/src/main/java/de/wnill/heat/core/services/SensorService.java
@@ -7,31 +7,31 @@ import de.wnill.heat.core.dto.Sensor;
 
 public class SensorService {
 
-	private ArrayList<Sensor> sensors = new ArrayList<Sensor>();
+  private ArrayList<Sensor> sensors = new ArrayList<Sensor>();
 
-	private static SensorService instance = null;
+  private static SensorService instance = null;
 
-	public static SensorService getInstance() {
-		if (instance == null) {
-			instance = new SensorService();
-		}
-		return instance;
-	}
+  public static SensorService getInstance() {
+    if (instance == null) {
+      instance = new SensorService();
+    }
+    return instance;
+  }
 
-	public void addSensor(String id) {
-		sensors.add(new Sensor(id));
-	}
+  public void addSensor(String id) {
+    sensors.add(new Sensor(id));
+  }
 
-	public List<Sensor> getAll() {
-		return sensors;
-	}
+  public List<Sensor> getAll() {
+    return sensors;
+  }
 
-	public Sensor getById(String id) {
-		for (Sensor sensor : sensors) {
-			if (sensor.getId().equals(id)) {
-				return sensor;
-			}
-		}
-		return null;
-	}
+  public Sensor getById(String id) {
+    for (Sensor sensor : sensors) {
+      if (sensor.getId().equals(id)) {
+        return sensor;
+      }
+    }
+    return null;
+  }
 }
