1c69311555928bd9838c4849bbfa8cbae39d64bd
Wladi <mail@wnill.de>
2015-11-30 10:12:09 +0100

    all resources as of current state

---
+++ Arduino/Arduino/sketches/HeatActuator/HeatActuator.ino
@@ -0,0 +1,176 @@
+
+// *** Config here
+const int IA_PIN = 5;
+const int IB_PIN = 6;
+const int READ_PIN = A0;
+// How many samples are read to calculate the (average) current
+const int SAMPLES = 5;
+// in ms
+const int SLEEP_BETWEEN_SAMPLES = 100;
+const double STALL_THRESHOLD = 0.03;
+
+unsigned long interval = 500;
+
+// *** Variables
+int mVperAmp = 185;
+int rawValue= 0;
+int acsOffset = 2500; 
+double voltage = 0;
+double amps = 0;
+float sample[SAMPLES];
+double avgAmp = 0;
+// deviation from 0V measured when motor is idle
+double normalize = .0;
+
+byte speed = 255;
+unsigned long lastCheck;
+
+String direction = "";
+char command;
+ 
+void setup() 
+{ 
+ Serial.begin(9600);
+  pinMode(IA_PIN, OUTPUT);
+  pinMode(IB_PIN, OUTPUT);
+  lastCheck = millis();
+
+  // First, normalize the current reading to 0
+  normalize = readCurrent();
+}
+ 
+void loop()
+{ 
+  if (Serial.available()) {
+    direction = "";
+    command = Serial.read();
+    direction.concat(command);
+
+    if (direction == "c") {
+      closeValve();
+    } else if (direction == "s") {
+      stop();
+    } else if (direction == "o") {
+      openValve();
+    } else if (direction == "i") {
+      calibrate();
+    }
+
+
+  }
+
+   double current = readCurrent();
+   if (current > STALL_THRESHOLD) {
+    Serial.println("STALL!");
+    stop();
+   }
+
+   if (millis() - lastCheck > interval) {
+     Serial.println(current, 2);
+     lastCheck = millis();
+   }   
+}
+
+void calibrate() {
+
+
+  unsigned long start = 0;
+  unsigned long stop = 0;
+  unsigned long avgOpening = 0;
+  unsigned long avgClosing = 0;
+  double current = .0;
+    
+  // start with a completely open valve
+  openValve();
+  while(current <= STALL_THRESHOLD) {
+    current = readCurrent();
+    Serial.println(current);
+    delay(200);
+  }
+
+  // cooldown
+  delay(1000);
+  
+  for (int i = 0; i < 3; i++) {
+    
+    Serial.println("Start calibration run");
+    double current = .0;
+    
+    // Start measuring the time!
+    start = millis();
+    closeValve();
+    while(current <= STALL_THRESHOLD) {
+      current = readCurrent();
+      Serial.println(current);
+      delay(200);
+    }
+    stop = millis();
+    Serial.println("Valve closed. Opening");
+    avgClosing += (stop - start);
+
+    // Cool down
+    delay(1000);
+
+    current = .0;
+
+    start = millis();
+    openValve();
+    while(current <= STALL_THRESHOLD) {
+      current = readCurrent();
+      Serial.println(current);
+      delay(200);
+    }
+    stop = millis();
+    Serial.println("Valve open");
+    avgOpening += (stop - start);
+
+    // Cool down
+    delay(1000);
+        
+  }
+
+  avgClosing = avgClosing / 3;
+  avgOpening = avgOpening / 3;
+
+  Serial.print("Average Closing in ms: ");
+  Serial.println(avgClosing);
+
+    Serial.print("Average Opening in ms: ");
+  Serial.println(avgOpening);
+
+}
+
+
+
+// Returns current in Amps
+double readCurrent() {
+  avgAmp = 0;
+
+  for (int it=0; it < SAMPLES; it++) {
+    rawValue = analogRead(READ_PIN);
+    delay(SLEEP_BETWEEN_SAMPLES);
+    voltage = (rawValue / 1023.0) * 5000; // Gets mV
+    amps = ((voltage - acsOffset) / mVperAmp);
+    avgAmp = avgAmp + amps;
+  }
+
+  double out = avgAmp / 10.;
+  out -= normalize;  
+  return out;
+}
+
+
+void closeValve() {
+  analogWrite(IA_PIN, speed);
+  analogWrite(IB_PIN, 0);
+}
+
+void openValve() {
+  analogWrite(IA_PIN, 0);
+  analogWrite(IB_PIN, speed);
+}
+
+void stop() {
+  analogWrite(IA_PIN, 0);
+  analogWrite(IB_PIN, 0);
+}
---
+++ Arduino/Arduino/sketches/TempSensor/TempSensor.ino
@@ -0,0 +1,123 @@
+/**
+ * The MySensors Arduino library handles the wireless radio link and protocol
+ * between your home built sensors/actuators and HA controller of choice.
+ * The sensors forms a self healing radio network with optional repeaters. Each
+ * repeater and gateway builds a routing tables in EEPROM which keeps track of the
+ * network topology allowing messages to be routed to nodes.
+ *
+ * Created by Henrik Ekblad <henrik.ekblad@mysensors.org>
+ * Copyright (C) 2013-2015 Sensnology AB
+ * Full contributor list: https://github.com/mysensors/Arduino/graphs/contributors
+ *
+ * Documentation: http://www.mysensors.org
+ * Support Forum: http://forum.mysensors.org
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * version 2 as published by the Free Software Foundation.
+ *
+ *******************************
+ *
+ * DESCRIPTION
+ *
+ * Example sketch showing how to send in DS1820B OneWire temperature readings back to the controller
+ * http://www.mysensors.org/build/temp
+ */
+
+#include <MySensor.h>  
+#include <SPI.h>
+#include <DallasTemperature.h>
+#include <OneWire.h>
+
+#define ONE_WIRE_BUS 3 // Pin where dallase sensor is connected 
+#define READS 5
+unsigned long SLEEP_BETWEEN_READS = 500; // Sleep time between reads (in milliseconds)
+unsigned long SLEEP_BETWEEN_TRANSMITS = 5000; // Sleep time between reads (in milliseconds)
+OneWire oneWire(ONE_WIRE_BUS); // Setup a oneWire instance to communicate with any OneWire devices (not just Maxim/Dallas temperature ICs)
+DallasTemperature sensors(&oneWire); // Pass the oneWire reference to Dallas Temperature. 
+MySensor gw;
+float sample[READS];
+float lastTemperature;
+int numSensors=0;
+boolean receivedConfig = false;
+boolean metric = true; 
+// Initialize temperature message
+MyMessage msg(0,V_TEMP);
+
+void setup()  
+{ 
+  Serial.begin(115200);
+  
+  // Startup up the OneWire library
+  sensors.begin();
+  // requestTemperatures() will not block current thread
+  sensors.setWaitForConversion(false);
+
+  // Startup and initialize MySensors library. Set callback for incoming messages. 
+  gw.begin();
+
+  // Send the sketch version information to the gateway and Controller
+  gw.sendSketchInfo("Temperature Sensor", "2.0");
+
+  // Fetch the number of attached temperature sensors  
+  numSensors = sensors.getDeviceCount();
+
+  // Present all sensor to controller
+  gw.present(0, S_TEMP);  
+}
+
+
+void loop()     
+{     
+  // Process incoming messages (like config from server)
+  gw.process(); 
+
+  // To improve precision and smooth out frequently changing measurements, take multiple samples and send the average as current temperature 
+  for (int it=0; it < READS; it++) {
+
+    // Fetch temperatures from Dallas sensors
+    sensors.requestTemperatures();
+
+    // query conversion time and sleep until conversion completed
+    int16_t conversionTime = sensors.millisToWaitForConversion(sensors.getResolution());
+    // sleep() call can be replaced by wait() call if node need to process incoming messages (or if node is repeater)
+    gw.sleep(conversionTime);
+
+    // Fetch and round temperature to one decimal
+    float temperature = static_cast<float>(static_cast<int>((gw.getConfig().isMetric?sensors.getTempCByIndex(0):sensors.getTempFByIndex(0)) * 10.)) / 10.;
+
+    Serial.print("Read");
+    Serial.print(it);
+    Serial.print(":");
+    Serial.println(temperature);
+
+    sample[it] = temperature;
+    gw.sleep(SLEEP_BETWEEN_READS);
+  }
+
+
+  int totalRead = 0;
+  float temperature = 0;
+  for (int i=0; i<READS; i++) {
+    // if there was no error
+    if (sample[i] != -127.00 && sample[i] != 85.00) {
+      temperature += sample[i];
+      totalRead++; 
+    }
+  }
+
+  if (totalRead > 0) {
+    float avgTemp = round(temperature / totalRead * 10.) / 10.; 
+    Serial.print("Avg Temp: ");
+    Serial.println(avgTemp);
+    if (avgTemp != lastTemperature) {
+      // Send in the new temperature
+      Serial.print("Sendind Update to Controller");
+      gw.send(msg.setSensor(0).set(avgTemp,1));
+      // Save new temperatures for next compare
+      lastTemperature=avgTemp;
+    }
+  }
+  
+  gw.sleep(SLEEP_BETWEEN_TRANSMITS);
+}
